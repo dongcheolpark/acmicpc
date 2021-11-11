be5b5ce51af14cd14ecc94125a554d5c80207ce5
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2021-11-11 11:13:24 +0900

    a

--- 1967/a.cpp
+++ 1967/a.cpp
@@ -4,31 +4,74 @@
 #include <queue>
 #include <cstring>
 #include <algorithm>
+#include <string>
+#include <tuple>
 #include <utility>
+
+#define check(X,Y,N,M) (X < 0 || X >= N || Y < 0 || Y >= M) ? 0 : 1
 
 using namespace std;
 
-int n;
-vector<pair<int,int> > v[10001];
-int arr[10001][10001] = {0,};
-
-void dfs(int a) {
-	for(int i = 0;i<v[a].size();i++) {
-	}
-}
+bool visit[1001][1001][2];
+int arr[1001][1001];
+int n,m;
 
 int main() {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
-
-	cin>>n;
+	cin>>n>>m;
 	for(int i = 0;i<n;i++) {
-		int a,b,c;
-		cin>>a>>b>>c;
-		v[a].push_back(make_pair(b,c));
-		v[b].push_back(make_pair(a,c));
+		string tmp;
+		cin>>tmp;
+		for(int j = 0;j<m;j++) {
+			arr[i][j] = tmp[j]-'0';
+		}
 	}
-	for(int i = 0;i<n;i++) {
+	queue<tuple<int,int,bool> > q;
+	q.push(make_tuple(0,0,0));
+	for(int i = 1;!q.empty();i++) {
+		int q_size = q.size();	
+		for(int j = 0;j<q_size;j++) {
+			auto tmp = q.front();
+			q.pop();
+			int x = get<0>(tmp),y = get<1>(tmp),chk = get<2>(tmp);
+			if(x == n-1 && y == m-1) {
+				cout<<i;
+				return 0;
+			}
+			if(visit[x][y][chk]) continue;
+			visit[x][y][chk] = 1;
+			if(check(x+1,y,n,m)&&arr[x+1][y] != 1) {
+				q.push(make_tuple(x+1,y,chk));
+			}
+			else if(check(x+1,y,n,m)&&!chk) {
+				q.push(make_tuple(x+1,y,1));
+			}
+
+			if(check(x-1,y,n,m)&&arr[x-1][y] != 1) {
+				q.push(make_tuple(x-1,y,chk));
+			}
+			else if(check(x-1,y,n,m)&&!chk) {
+				q.push(make_tuple(x-1,y,1));
+			}
+
+			if(check(x,y+1,n,m)&&arr[x][y+1] != 1) {
+				q.push(make_tuple(x,y+1,chk));
+			}
+			else if(check(x,y+1,n,m)&&!chk) {
+				q.push(make_tuple(x,y+1,1));
+			}
+
+			if(check(x,y-1,n,m)&&arr[x][y-1] != 1) {
+				q.push(make_tuple(x,y-1,chk));
+			}
+			else if(check(x,y-1,n,m)&&!chk) {
+				q.push(make_tuple(x,y-1,1));
+			}
+
+		}
 
 	}
+	cout<<-1;
+	return 0;
 }
