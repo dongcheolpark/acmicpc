00c4baf1b0550e051098c7595d1b6387009510cb
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-03-14 14:38:19 +0900

    2252

--- 2252/a.cpp
+++ 2252/a.cpp
@@ -11,12 +11,13 @@ int main() {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
 	vector<vector<int>> linknodes;
-	vector<int> count;
+	vector<int> count,visit;
 
 	int n,m;
 	cin>>n>>m;
 	linknodes.resize(n+1);
 	count.resize(n+1);
+	visit.resize(n+1);
 	for(int i = 0;i<m;i++) {
 		int a,b;
 		cin>>a>>b;
@@ -25,10 +26,21 @@ int main() {
 	}
 	queue<int> q;
 	while(1) {
+		int chk = true;
 		for(int i = 1;i<=n;i++) {
-			if(count[i] == 0) {
-				
+			if(count[i] == 0&&visit[i] == 0) {
+				visit[i] = 1;
+				q.push(i);
+				chk = false;
+				for(auto item : linknodes[i]) {
+					count[item]--;
+				}
 			}
+		}
+		if(chk) break;;
+		while(!q.empty()) {
+			cout<<q.front()<<' ';
+			q.pop();
 		}
 	}
 }
