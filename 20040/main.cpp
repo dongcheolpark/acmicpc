0638cccb73b251f3839af78d2fdb0047fe25e321
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-05-07 18:51:12 +0900

    20040

--- 20040/main.cpp
+++ 20040/main.cpp
@@ -4,24 +4,37 @@
 using namespace std;
 
 int n,m;
-vector<int> union_find;
+int union_find[500000];
 
-void Union() {
-
+int Find(int x) {
+	if(x == union_find[x]) return x;
+	return union_find[x] = Find(union_find[x]);
 }
-void Find() {
 
+bool Union(int x,int dest) {
+	int a = Find(x);	
+	int b = Find(dest);	
+	if(a == b) return true;
+	if(x < dest) union_find[x] = dest;
+	else union_find[dest] = x;
+	return false;
 }
 
 int main() {
 	cin.tie(NULL);
 	ios::sync_with_stdio(false);
 	cin>>n>>m;	
-	union_find.resize(n);
 	for(int i = 0;i<n;i++) {
 		union_find[i] = i;
 	}
 	for(int i = 0;i<m;i++) {
-
+		int a,b;
+		cin>>a>>b;
+		if(Union(a,b)) {
+			cout<<i+1;
+			return 0;
+		}
 	}
+	cout<<0;
+	return 0;
 }
