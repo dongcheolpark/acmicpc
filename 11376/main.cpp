cdc17079e7b2a5b9581521b5daa4a061ba221fa7
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-11-03 12:27:26 +0900

    11376

---
+++ 11376/main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+int n,m;
+vector<vector<int>> v;	
+vector<int> A,B;
+vector<bool> vis;
+
+bool func(int x) {
+	vis[x] = true;
+	for(int b : v[x]) {
+		if(B[b] == -1 || !vis[B[b]] && func(B[b])) {
+			B[b] = x;
+			A[x] = b;
+			return true;
+		}
+	}
+	return false;
+}
+
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin>>n;
+	cin>>m;
+	n*=2;
+	A.resize(n,-1);
+	B.resize(m,-1);
+	v.resize(n);
+	vis.resize(n,false);
+
+	for(int i = 0;i<n/2;i++) {
+		int k,tmp;
+		cin>>k;
+		for(int j = 0;j<k;j++) {
+			cin>>tmp;
+			v[i*2].push_back(tmp-1);
+			v[i*2+1].push_back(tmp-1);
+		}
+	}
+	int res = 0;
+
+	for(int i = 0;i<n;i++) {
+		if(A[i] == -1) {
+			fill(vis.begin(),vis.end(),false);
+			if(func(i)) res++;
+		}
+	}
+	cout<<res<<'\n';
+
+	return 0;
+}
