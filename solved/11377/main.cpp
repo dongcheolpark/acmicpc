5853732bb27d8d376b540652c84e2dd7432d9c59
박동철 <36955431+dongcheolpark@users.noreply.github.com>
2022-11-04 20:03:06 +0900

    11377

---
+++ solved/11377/main.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int n,m,k;
+vector<vector<int>> v;	
+vector<int> A,B;
+vector<bool> vis;
+
+bool func(int x) {
+	vis[x] = true;
+	for(int b : v[x]) {
+		if(B[b] == -1 || !vis[B[b]] && func(B[b])) {
+			B[b] = x;
+			A[x] = b;
+			return true;
+		}
+	}
+	return false;
+}
+
+
+int main() {
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+	cin>>n;
+	cin>>m;
+	cin>>k;
+	A.resize(n,-1);
+	B.resize(m,-1);
+	v.resize(n);
+	vis.resize(n,false);
+
+	for(int i = 0;i<n;i++) {
+		int k,tmp;
+		cin>>k;
+		for(int j = 0;j<k;j++) {
+			cin>>tmp;
+			v[i].push_back(tmp-1);
+		}
+	}
+
+	int res = 0;
+	for(int i = 0;i<n; i++) {
+		if(A[i] == -1) {
+			fill(vis.begin(),vis.end(),false);
+			if(func(i)) res++;
+		}
+	}
+
+	int cnt = 0;
+
+	while(1) {
+		bool flag = false;
+		for(int i = 0;i<n && cnt<k; i++) {
+			fill(vis.begin(),vis.end(),false);
+			if(func(i)) {
+				res++;
+				flag = true;
+				cnt++;
+				break;
+			}
+		}
+		if(!flag) break;
+	}
+	cout<<res<<'\n';
+
+	return 0;
+}
